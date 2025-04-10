c6cb62f875982a4aec3c9b26c047dd42fcd563c2
Alex Langevin <alexlangev@gmail.com>
2025-04-10 00:14:11 -0400

    todo base logic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module alexlangev/go-todo-cli
+
+go 1.24.2
---
+++ todo.go
@@ -0,0 +1,55 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutils"
+	"os"
+	"time"
+)
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type List []item
+
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*l = append(*l, t)
+}
+
+func (l *List) Complete(i int) error {
+	ls := *l
+
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+func (l *List) Delete(i int) error {
+	ls := *l
+
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	} 
+
+	*l = append(ls[:i-1], ls[i:]...)
+
+	return nil
+}
