af6ee0e88847be8076b409af93849fc8dd6ad989
Alex Langevin <alexlangev@gmail.com>
2025-04-23 00:03:17 -0400

    read from STDIN

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"io"
+	"strings"
 
 	"alexlangev/go-todo-cli"
 )
 
-const todoFileName = ".todo.json"
+var todoFileName = ".todo.json"
 
 func main() {
+	// Check if the user defined the ENV VAR for a custom file name
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
+
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "%s tool. Developed for practice\n", os.Args[0])
 		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2025\n")
@@ -18,7 +26,7 @@ func main() {
 		flag.PrintDefaults()
 	}
 
-	task := flag.String("task", "", "Task to be included in the ToDo list")
+	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 
@@ -35,11 +43,6 @@ func main() {
 
 	case *list:
 		fmt.Print(l)
-		// for _, item := range *l {
-		// 	if !item.Done {
-		// 		fmt.Println(item.Task)
-		// 	}
-		// }
 
 	case *complete > 0:
 		if err := l.Complete(*complete); err != nil {
@@ -52,8 +55,16 @@ func main() {
 			os.Exit(1)
 		}
 
-	case *task != "":
-		l.Add(*task)
+	case *add:
+		// When any arguments (excluding flags) are provided, they will be used as the new task
+		t, err := getTask(os.Stdin, flag.Args()...)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		l.Add(t)
 
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -66,4 +77,23 @@ func main() {
 		os.Exit(1)
 	}
 
+}
+
+func getTask(r io.Reader, args ...string)(string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+
+	return s.Text(), nil
 }
